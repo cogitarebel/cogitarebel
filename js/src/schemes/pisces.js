b54a1a7af9faf3c151e42b3850363302f5ea554c
cogitarebel <zazartsai@gmail.com>
2018-01-03 11:54:47 +0800

    Site updated: 2018-01-03 11:54:46

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 25 2017 16:04:30 GMT+0800 (CST)
+// build time:Wed Jan 03 2018 11:54:39 GMT+0800 (CST)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
