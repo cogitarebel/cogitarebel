451b328e5245a6008193df5efa6b48f09a5b3a09
cogitarebel <zazartsai@gmail.com>
2018-01-07 18:20:10 +0800

    Site updated: 2018-01-07 18:20:09

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Wed Jan 03 2018 11:54:39 GMT+0800 (CST)
+// build time:Sun Jan 07 2018 18:19:42 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
